d65c8a05a2828d3cb80ea03508723bcfb3d48e1a
Mauricio Togneri <mauricio.togneri@gmail.com>
2023-07-07 20:50:25 +0200

    Improvements

---
+++ scripts/run.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+set -e
+
+BASE_DIR=`dirname $0`
+
+function convert()
+{
+    inkscape --export-width=375 --export-height=525 --export-type=png --export-filename="${BASE_DIR}/../png/${1}.png" "${BASE_DIR}/../svg/${1}.svg"
+}
+
+node ${BASE_DIR}/script.js
+
+convert "crew/pusher/pusher1"
