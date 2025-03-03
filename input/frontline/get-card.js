b84b77cca1cdd7fc91b807f59067191d9460101c
Mauricio Togneri <mauricio.togneri@gmail.com>
2025-03-03 20:33:41 +0100

    Improvements

--- input/frontline/get-card.js
+++ input/frontline/get-card.js
@@ -1,7 +1,19 @@
+const fs = require('fs')
+
+function base64File(path) {
+    const content = fs.readFileSync(path, { encoding: 'base64' })
+
+    return 'data:image/png;base64,' + content
+}
+
+function cardImage(name) {
+    return base64File(`input/frontline/images/${name}.png`)
+}
+
 function generateSvg(_, template, title, faction, frame, image, level, _, _) {
     const card = template
         .replace('{{TITLE}}', title)
-        .replace('{{FACTION}}', faction)
+        .replace('{{FACTION}}', cardImage(faction))
         .replace('{{LEVEL}}', level)
         .replace('{{FRAME}}', frame)
         .replace('{{IMAGE}}', image)
